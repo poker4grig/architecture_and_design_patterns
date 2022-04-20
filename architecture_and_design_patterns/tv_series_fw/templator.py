d64eec379452611f50303b81c926be4ff113ba5d
Sergey <poker4grig@yandex.ru>
2022-04-20 18:53:08 +0300

    add common templates with inheritance

--- architecture_and_design_patterns/tv_series_fw/templator.py
+++ architecture_and_design_patterns/tv_series_fw/templator.py
@@ -1,11 +1,21 @@
-from jinja2 import Template
-import os
+from jinja2 import Template, FileSystemLoader
+from jinja2.environment import Environment
 
 
 def render(template_name, template_folder='templates', **kwargs):
-    path = os.path.join(template_folder, template_name)
-
-    with open(path, encoding='utf-8') as file:
-        template = Template(file.read())
-
+    env = Environment()
+    env.loader = FileSystemLoader(template_folder)
+    template = env.get_template(template_name)
     return template.render(**kwargs)
+
+
+# import os
+#
+#
+# def render(template_name, template_folder='templates', **kwargs):
+#     path = os.path.join(template_folder, template_name)
+#
+#     with open(path, encoding='utf-8') as file:
+#         template = Template(file.read())
+#
+#     return template.render(**kwargs)
