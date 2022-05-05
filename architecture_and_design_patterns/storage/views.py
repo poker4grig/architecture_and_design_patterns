48131ac1120f5771c86db89988ba3f9c4848cbe9
Sergey <poker4grig@yandex.ru>
2022-05-05 18:13:00 +0300

    add structural patterns for project

--- architecture_and_design_patterns/storage/views.py
+++ architecture_and_design_patterns/storage/views.py
@@ -1,24 +1,35 @@
 from datetime import date
-import urllib.parse
 
 from tv_series_fw.templator import render
 from patterns.generative_patterns import Interface, Logger
+from patterns.structural_patterns import AppRoute, Debug
 
 site = Interface()
+site.categories.append(site.create_category('Драма'))
+site.categories.append(site.create_category('Боевик'))
+site.categories.append(site.create_category('Фантастика'))
+site.categories.append(site.create_category('Комедия'))
+
 site.free_users = ['Николай Николаев', 'Петр Петров', 'Иван Иванов']
 site.subscribed_users = ['Александр Александров', 'Василий Васильев',
                          'Сергей Сергеев']
 
 logger = Logger('main')
 
+routes = dict()
 
+
+@AppRoute(routes=routes, url='/')
 class Index:
+    @Debug(name='Index')
     def __call__(self, request):
         # return '200 OK', render('index.html', style=request.get('style', None))
         return '200 OK', render('index.html', objects_list=site.categories)
 
 
+@AppRoute(routes=routes, url='/about/')
 class About:
+    @Debug(name='About')
     def __call__(self, request):
         # return '200 OK', render('about.html', style=request.get('style', None))
         return '200 OK', render('about.html')
@@ -29,17 +40,22 @@ class PageNotExists:
         return '404 WHAT', '404 PAGE Not Found'
 
 
+@AppRoute(routes=routes, url='/series-schedule/')
 class SeriesSchedule:
     """Расписание сериалов"""
+    @Debug(name='SeriesSchedule')
     def __call__(self, request):
         return '200 OK', render('series-schedule.html',  data=date.today())
 
 
+@AppRoute(routes=routes, url='/series-list/')
 class SeriesList:
+    @Debug(name='SeriesList')
     def __call__(self, request):
         logger.log("Список сериалов")
         try:
-            category = site.find_category_by_id(int(request['request_params']['id']))
+            category = site.find_category_by_id(int(request['request_params']
+                                                    ['id']))
             return '200 OK', render('series-list.html',
                                     objects_list=category.serieses,
                                     name=category.name, id=category.id)
@@ -47,10 +63,12 @@ class SeriesList:
             return '200 OK', 'No series have been added yet'
 
 
+@AppRoute(routes=routes, url='/create-series/')
 class CreateSeries:
     """Создать сериал"""
     category_id = -1
 
+    @Debug(name='CreateSeries')
     def __call__(self, request):
         if request['method'] == 'POST':
             data = request['data']
@@ -79,8 +97,9 @@ class CreateSeries:
                 return '200 OK', 'No categories have been added yet'
 
 
+@AppRoute(routes=routes, url='/create-category/')
 class CreateCategory:
-
+    @Debug(name='CreateCategory')
     def __call__(self, request):
 
         if request['method'] == 'POST':
@@ -105,16 +124,22 @@ class CreateCategory:
             return '200 OK', render('index.html', objects_list=site.categories)
         else:
             categories = site.categories
-            return '200 OK', render('create-category.html', categories=categories)
+            return '200 OK', render('create-category.html',
+                                    categories=categories)
 
 
+@AppRoute(routes=routes, url='/category-list/')
 class CategoryList:
+    @Debug(name='CategoryList')
     def __call__(self, request):
         logger.log('Список категорий')
-        return '200 OK', render('category-list.html', objects_list=site.categories)
+        return '200 OK', render('category-list.html',
+                                objects_list=site.categories)
 
 
+@AppRoute(routes=routes, url='/copy-series/')
 class CopySeries:
+    @Debug(name='CopySeries')
     def __call__(self, request):
         request_params = request['request_params']
 
@@ -127,16 +152,23 @@ class CopySeries:
                 new_series.name = new_name
                 site.serieses.append(new_series)
 
-            return '200 OK', render('series-list.html', objects_list=site.serieses)
+            return '200 OK', render('series-list.html',
+                                    objects_list=site.serieses)
         except KeyError:
             return '200 OK', 'No series have been added yet'
 
 
+@AppRoute(routes=routes, url='/contacts/')
 class Contacts:
+    @Debug(name='Contacts')
     def __call__(self, request):
-        return '200 OK', render('contacts.html', style=request.get('style', None))
+        return '200 OK', render('contacts.html', style=request.get('style',
+                                                                   None))
 
 
+@AppRoute(routes=routes, url='/watch/')
 class Watch:
+    @Debug(name='Watch')
     def __call__(self, request):
-        return '200 OK', render('watch.html', style=request.get('style', None), content=request.get('series', None))
+        return '200 OK', render('watch.html', style=request.get('style', None),
+                                content=request.get('series', None))
