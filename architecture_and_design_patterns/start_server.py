9c55f116d4e043b5bae73be782f079f703bbe2c9
Sergey <poker4grig@yandex.ru>
2022-05-28 09:44:01 +0300

    add static

--- architecture_and_design_patterns/start_server.py
+++ architecture_and_design_patterns/start_server.py
@@ -1,3 +1,6 @@
+import os
+
+from wsgi_static_middleware import StaticMiddleware
 from wsgiref.simple_server import make_server
 from tv_series_fw.my_app import MyFramework
 from storage.other_wsgi import DebugApp, FakeApp
@@ -6,13 +9,23 @@ from storage.views import routes
 from storage.middlewares import middlewares
 
 
+ROOT_DIR = os.path.dirname(__name__)
+STATIC_DIRS = [os.path.join(ROOT_DIR, 'staticfiles')]
 application = MyFramework(routes, middlewares)
+
+app_static = StaticMiddleware(application,
+                              static_root='staticfiles',
+                              static_dirs=STATIC_DIRS)
 
 # application = DebugApp(routes, middlewares)
 
 # application = FakeApp(routes, middlewares)
 
-with make_server('', 8000, application) as server:
-    print('...running server...')
-    server.serve_forever()
+# with make_server('', 8000, application) as server:
+#     print('...running server...')
+#     server.serve_forever()
+
+with make_server('', 8000, app_static) as httpd:
+    print("Запуск на порту 8000...")
+    httpd.serve_forever()
 
--- architecture_and_design_patterns/tv_series_fw/templator.py
+++ architecture_and_design_patterns/tv_series_fw/templator.py
@@ -2,9 +2,10 @@ from jinja2 import Template, FileSystemLoader
 from jinja2.environment import Environment
 
 
-def render(template_name, template_folder='templates', **kwargs):
+def render(template_name, template_folder='templates', static_url='/static/', **kwargs):
     env = Environment()
     env.loader = FileSystemLoader(template_folder)
+    env.globals['static'] = static_url
     template = env.get_template(template_name)
     return template.render(**kwargs)
 
